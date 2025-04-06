3117fef5e3dfbc99a9b98327783635a3b4429705
J-Ahmed-Gaafar <ahmed.fhesham@jumia.com>
2025-04-06 10:09:12 +0200

    cards - Custom Type Declarations

--- cards/main.go
+++ cards/main.go
@@ -1,18 +1,18 @@
 package main
 
-import "fmt"
-
 func main() {
 	//var card = string = "Ace of Spades"
 	//card := "Ace of Spades"
 	//card = "Five of Diamonds"
 
-	cards := []string{"Ace of Diamonds", newCard()}
+	//cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	//for i, card := range cards {
+	//	fmt.Println(i, card)
+	//}
+	cards.print()
 }
 
 func newCard() string {
