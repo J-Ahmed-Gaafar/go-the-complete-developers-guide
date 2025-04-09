b46482eea4bdaec7cf7b2c6bc171eb07aae5b056
J-Ahmed-Gaafar <ahmed.fhesham@jumia.com>
2025-04-09 13:40:30 +0200

    Manipulating Maps

--- map/main.go
+++ map/main.go
@@ -3,10 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#00ff00",
-	}
+	//var colors map[string]string
+	//colors := make(map[string]string)
+	colors := make(map[int]string)
+
+	//colors := map[string]string{
+	//	"red":   "#ff0000",
+	//	"green": "#00ff00",
+	//}
+
+	//colors["white"] = "#ffffff"
+	colors[10] = "ffffff"
+
+	delete(colors, 10)
 
 	fmt.Println(colors)
 }
