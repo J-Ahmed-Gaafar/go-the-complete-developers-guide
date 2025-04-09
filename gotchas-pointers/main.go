39668098697f6b0dc276ec0fad374cee4fafbc87
J-Ahmed-Gaafar <ahmed.fhesham@jumia.com>
2025-04-09 10:42:18 +0200

    Gotchas with Pointers

---
+++ gotchas-pointers/main.go
@@ -0,0 +1,15 @@
+// Originally implemented on Go Playground
+package main
+
+import "fmt"
+
+func main() {
+	mySlice := []string{"Hi", "There", "How", "Are", "You"}
+
+	updateSlice(mySlice)
+	fmt.Println(mySlice)
+}
+
+func updateSlice(s []string) {
+	s[0] = "Bye"
+}
