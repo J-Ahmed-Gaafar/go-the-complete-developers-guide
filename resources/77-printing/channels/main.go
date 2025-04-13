959ebabdf8f19386a3e69cc62506aa14ff5dde70
J-Ahmed-Gaafar <ahmed.fhesham@jumia.com>
2025-04-13 11:09:55 +0200

    Printing Site Status

--- resources/77-printing/channels/main.go
+++ resources/77-printing/channels/main.go
@@ -7,23 +7,24 @@ import (
 
 func main() {
 	links := []string{
-		"https://google.com",
-		"https://facebook.com",
-		"https://stackoverflow.com",
-		"https://golang.org",
-		"https://amazon.com",
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
 	}
 
 	for _, link := range links {
 		checkLink(link)
 	}
+
 }
 
 func checkLink(link string) {
 	_, err := http.Get(link)
-
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		return
 	}
 
 	fmt.Println(link, "is up!")
