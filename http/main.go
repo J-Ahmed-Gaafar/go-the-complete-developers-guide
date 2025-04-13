d755b248f40b2d6e015539e032f08b8b425e2c4d
J-Ahmed-Gaafar <ahmed.fhesham@jumia.com>
2025-04-13 06:29:32 +0200

    A Custom Writer

--- http/main.go
+++ http/main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("https://google.com")
 	if err != nil {
@@ -20,5 +22,15 @@ func main() {
 	//resp.Body.Read(bs)
 	//fmt.Println(string(bs))
 
-	io.Copy(os.Stdout, resp.Body)
+	//io.Copy(os.Stdout, resp.Body)
+
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	//return 1, nil // Wrong Implementation that compiles correctly
+	fmt.Println(string(bs))
+	fmt.Println("just wrote", len(bs), "bytes")
+	return len(bs), nil // Correct Implementation
 }
