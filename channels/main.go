85da6727f86ac6000f05329a83337e748d6a0fa3
J-Ahmed-Gaafar <ahmed.fhesham@jumia.com>
2025-04-17 09:06:18 +0200

    Blocking Channels

--- channels/main.go
+++ channels/main.go
@@ -21,6 +21,11 @@ func main() {
 	}
 
 	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	//fmt.Println(<-c) // Will hang up the program
 }
 
 func checkLink(link string, c chan string) {
