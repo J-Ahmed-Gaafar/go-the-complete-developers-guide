5550dbfff1e5aa9b39f181dfd4077c5d6dd9d925
J-Ahmed-Gaafar <ahmed.fhesham@jumia.com>
2025-04-17 10:23:05 +0200

    Function Literals

--- channels/main.go
+++ channels/main.go
@@ -33,13 +33,18 @@ func main() {
 	// }
 
 	for l := range c {
-		time.Sleep(5 * time.Second)
-		go checkLink(l, c)
+		//time.Sleep(5 * time.Second)
+		//go checkLink(l, c)
+		go func() {
+			time.Sleep(5 * time.Second)
+			checkLink(l, c)
+		}()
 	}
 }
 
 func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
+	time.Sleep(5 * time.Second)
 
 	if err != nil {
 		fmt.Println(link, "might be down!")
